63580737aee3fbc1606ad096598d37fef3e6dd30
Sorangel <54912992+sorangel@users.noreply.github.com>
2021-12-28 23:44:05 -0500

    Configuracion del frontend con handlebars

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,11 +1,25 @@
 // @ts-ignore
-import express from "express";
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
+import { engine } from "express-handlebars";
+import * as path from "path";
 
 const app = express();
 
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+    layoutsDir: path.resolve(__dirname, "../frontend/views/layouts"),
+    partialsDir: path.resolve(__dirname, "../frontend/views/partials"),
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.set("views", path.resolve(__dirname, "../frontend/views"));
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+  res.render("home", { layout: "default", template: "home-template" });
 });
 
 const PORT = process.env.PORT || 3000;
